462f900936253147077b6df84fefa079fef79c34
Cristaling-LAP <iovarares@gmail.com>
2017-03-12 07:05:00 +0200

    Commit3.5

--- FieldCareApp/AddingController.js
+++ FieldCareApp/AddingController.js
@@ -1,14 +1,23 @@
 (function(){
 	'use strict'
 	var app = angular.module('FarmCare');
-	app.controller('AddingController', ['$location', function($location)
+	app.controller('AddingController', ['$location', '$cookies', function($location, $cookies)
 	{
 		var vm = this
-		
-		vm.getToAddPage = getToAddPage;
-		
-		function getToAddPage(){
-			$location.path('/addfield');
+		vm.entry = {};
+		vm.Adauga = Adauga;
+
+		function Adauga(){
+			var cookie = $cookies.getAll();
+			if('FieldsToCare' in cookie){
+				var entries = $cookies.getObject('FieldsToCare');
+			}else{
+				var entries = [];
+			}
+			console.log(entries);
+			entries.push(vm.entry);
+			$cookies.putObject('FieldsToCare', entries);
+			$location.path('/landing');
 		}
 	}]);
 })();
--- FieldCareApp/LandingController.js
+++ FieldCareApp/LandingController.js
@@ -1,14 +1,27 @@
 (function(){
 	'use strict'
 	var app = angular.module('FarmCare');
-	app.controller('LandingController', ['$location', function($location)
+	app.controller('LandingController', ['$location', '$cookies', function($location, $cookies)
 	{
-		var vm = this
-		
+		var vm = this;
+		vm.fields = [];
+
+		var cookie = $cookies.getAll();
+		if('FieldsToCare' in cookie){
+			vm.fields = $cookies.getObject('FieldsToCare');
+		}
+
 		vm.getToAddPage = getToAddPage;
+		vm.removeField = removeField;
 		
 		function getToAddPage(){
 			$location.path('/addfield');
+		}
+
+		function removeField(field){
+			vm.fields = vm.fields.filter(function(el) {
+				return el !== field;
+			});
 		}
 	}]);
 })();
